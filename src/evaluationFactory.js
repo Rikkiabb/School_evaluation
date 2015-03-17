9b7ba289031f376c9719fd32a5d2c28babc4599a
Rikard Arnar B. Birgisson <rikardarnar@gmail.com>
2015-03-17 13:04:57 +0000

    Modifying conf

--- src/evaluationFactory.js
+++ src/evaluationFactory.js
@@ -3,20 +3,15 @@ angular.module("EvalApp").factory("evaluationFactory", ["$http", "SERVER", funct
 	return {
 
 		addEvalQuestion: function(course, semester, evalID, question){
-			console.log("addQ: ",question);
-			console.log("c: ", course, " s: ", semester, " e: ", evalID);
-			// $http.post(SERVER + "courses/" + course + "/" + semester + "/evaluations/" + evalID, question)
-			// 	.success(function(data){
-			// 		console.log("VIRKAR!!!!!");
-			// 	})
-			// 	.error(function(data, status){
-			// 		if(status === 401){
-			// 			console.log("FOKKUP");
-			// 		}
-			// 		else{
-			// 			console.log("FOKKUP status: ", status);
-			// 		}
-			// 	});
+			$http.post(SERVER + "courses/" + course + "/" + semester + "/evaluations/" + evalID, question)
+				.success(function(data){
+					console.log("VIRKAR!!!!!");
+				})
+				.error(function(data, status){
+					if(status === 401){
+						console.log("Error", status);
+					}
+				});
 		}
 	}
 
