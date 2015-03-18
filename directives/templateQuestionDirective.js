63cc4e2778ac1b68d123514e98f1e359508b923f
Haftor <haftor.g@.com>
2015-03-18 21:33:01 +0000

    fixing

--- directives/templateQuestionDirective.js
+++ directives/templateQuestionDirective.js
@@ -3,8 +3,7 @@ angular.module("EvalApp").directive("templateQuestion", function ($window, $comp
 	return {
 		restrict: "A",
 		scope: {
-			templ: "=templateQuestion",
-			lang: "=lang"
+			templ: "=templateQuestion"
 		},
 
 		link: function(scope, element, attr){
@@ -14,14 +13,8 @@ angular.module("EvalApp").directive("templateQuestion", function ($window, $comp
 				return "templates/" + scope.templ.Type + "TemplateQuestion.html";
 			}
 
-			if(scope.lang === 0){
-				scope.question = scope.templ.Text;
-				scope.langu = 0;
-			}
-			else if(scope.lang === 1){
-				scope.question = scope.templ.TextEN;
-				scope.langu = 1;
-			}		
+			scope.question = scope.templ.Text;
+	
 			scope.qType = scope.templ.Type;
 			
 			if(scope.qType === "multiple" || scope.qType === "single"){
